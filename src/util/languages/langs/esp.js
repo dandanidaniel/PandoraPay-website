92a4069fd4651b287b7d6642d0a30bfe0e48703d
d a n d a n i e l <60680263+dandanidaniel@users.noreply.github.com>
2020-02-05 06:27:26 +0100

    Update and rename en.js to esp.js
    Spanish

---
+++ src/util/languages/langs/esp.js
@@ -0,0 +1,167 @@
+import config from "build/config/config";
+
+export default {
+
+    global:{
+        webWallet: "Cartera WebWallet",
+    },
+
+    pages:{
+
+        homepage:{
+            title: "El Primer Exchange Anónimo y Descentralizado",
+            subTitle: "Pagos Anónimos hechos simples",
+            blocks: 'Bloques',
+            transactions: 'Transacctiones',
+            uniqueAccounts: 'Cuentas',
+            realTimeDataFrom: 'Datos en tiempo real',
+            testNetBlockchainExplorer: 'Test net blockchain explorador',
+
+            anonymousCashForTheInternet: 'Pago Anonimo en la Red',
+            worldsFirstAnonymousDecentralizedExchange: "El Primer Exchange Anónimo y Descentralizado",
+            itsFastPrivateAndSecure: `Es rapido, privado y seguro. Con ${config.app.name}, tu eres tu proprio banco. Puedes enviar dinero de forma totalmente anónima a cualquier persona sabiendo que otros no pueden ver su saldo o rastrear tu actividad.`,
+            
+            getPaidAnonymously: 'Recibe Pagos de forma Anónima',
+            itsUntraceable: "Es imposible de rastrear",
+            sendingReceivingAddressesObfuscated: `Las direcciones de envío y recepción, así como los montos de las transacciones, están ocultos de manera predeterminada. Las transacciones en ${config.app.name} no se pueden vincular a un usuario en particular o una identidad del mundo real.`,
+
+            privateDigitalCurrency: "CriptoMoneda digital privada",
+            withASingleClick: "con un solo Clic",
+            neverBeenEasierToSendAndReceiveMoney: "Nunca fue tan fácil enviar y recibir dinero internacionalmente de forma totalmente anónima e imposible de rastrear.",
+
+            youAreYourOwnBank: `Eres tu propio Banco`,
+            withNameYouAreYourOwnBank: `Con ${config.app.name}, tu eres tu propio banco y los demás no pueden ver su saldo ni rastrear su actividad. Saluda a ${config.app.name}.`,
+            nameIsSecure: `${config.app.name} es seguro`,
+            unstoppableByUsingDecentralizedBlockchain: `Imparable mediante el uso de tecnología blockchain descentralizada asegurada por una red de usuarios.`,
+            nameIsPrivate: `${config.app.name} es privado`,
+            youOwnYourOwnKey: `Usted posee su propia clave y nadie puede rastrearlo.`,
+
+            anonymousPaymentsMadeEasy: `Pagos anónimos de manera fácil.`,
+            webDWallet:`Cartera WebWallet`,
+
+            makeItPerfect:`¡ Haciéndolo perfecto para ti !`,
+            worldFirstAnonymousStablecoin:`La primera criptomoneda anónima e imposible de rastrear del mundo`,
+            
+            descriptionOfCharacterPrivate: `Privado`,
+            descriptionOfCharacterPrivateZether: `El nuevo mecanismo criptográfico Zether que utiliza Σ-bullets oculta los orígenes, cantidades y destinos de todas las transacciones.`,
+
+            descriptionOfCharacterUntraceable: `Imposible de rastrear`,
+            descriptionOfCharacterObfuscated: `Las direcciones de envío y recepción, incluidas las cantidades negociadas, se ocultan de manera predeterminada.`,
+
+            descriptionOfCharacterFungible: `intercambiable`,
+            descriptionOfCharacterNoBlacklist: `Las transacciones ${config.app.name} no pueden ser incluidas en listas negras por vendedores o casas de cambio debido a su asociación en transacciones anteriores.`,
+
+            descriptionOfCharacterAssetPlusCurrency: `Activos y monedas`,
+            descriptionOfCharacterAssetPlusCurrencyAnchor: `Activos digitales anónimos & monedas que anclan el valor de otros activos respaldados por la comunidad.`,
+
+            descriptionOfCharacterConfidentialBlindedEscrow: `Fideicomiso Ciego Confidencial`,
+            descriptionOfCharacterCompletelyBlindEscrow: `Los fideicomisos no saben la cantidad, el remitente real y el destino real. Los intermediarios son completamente ciegos.`,
+
+            descriptionOfCharacterReviewsForTraders: `Ratings para Traders`,
+            descriptionOfCharacterReviewsForTradersAnonymous: `Para aumentar la confianza entre los Traders en una red, se pueden escribir revisiones anónimas.`,
+
+            descriptionOfCharacterEncryptedChat: `Chat Encriptado`,
+            descriptionOfCharacterEncryptedChatEndToEnd: `El chat en ${config.app.name} entre los comerciantes se encripta de extremo a extremo.`,
+
+            descriptionOfCharacterNoDeposit: `Sin depósito`,
+            descriptionOfCharacterNoDepositNoCoins: `${config.app.name} es el primer intercambio p2p descentralizado que permite a los usuarios comenzar a operar sin necesidad de depositar o poseer monedas. Para comprar monedas o activos, no necesita poseer monedas`,
+
+            descriptionOfCharacterInstantAnonymity: `Anonimato instantáneo & HD Wallets`,
+            descriptionOfCharacterAdvancedHdWallet: `Para verificar sus propios saldos encriptados, solo lleva unos segundos descifrar sus propios saldos y sus transacciones anteriores. Más aún, ha sido implementado una cartera HD avanzada.`,
+
+            mainNetTimeline: `Timeline para PandoraPay Main Net`,
+            launchTimeline: `Timeline para el lanzamiento PandoraPay. No queremos ofrecer ninguna fecha específica, pero la mayoría del código ya se implementó.`,
+
+            featuresTimelineDataStructure: `Data structure`,
+            featuresTimelineNetworking: `Networking`,
+            featuresTimelineCryptoAndRingCT: `Crypto & RingCT - Ring Confidential Signatures`,
+            featuresTimelineBlockchainAndConsensus: `Blockchain & Consensus`,
+            featuresTimelineExchangeFiftyPerCent: `Exchange - 50%`,
+            featuresTimelineEncryptedMessages: `Encrypted Messages`,
+            featuresTimelineCompletlyBlindedEscrow: `Completly Blinded Escrow`,
+            featuresTimelineFinalTests: `Final Tests`,
+
+            gotAQuestion: `¿Tienes una pregunta? Tenemos respuestas en `,
+            gotAQuestionFaq: "Preguntas más frecuentes / FAQ",
+        },
+
+        contact:{
+            powerToThePeople: `People`,
+            powerToThePeopleMeetTheCommunity: `Meet us`,
+
+            aFewGoodMen: `Meet our small team that make those great products`,
+            aFewGoodMenAlexandruIonutBudisteanu: `Alexandru Ionut Budisteanu`,
+            aFewGoodMenAbi: `Alexandru Ionut Budisteanu`,
+            aFewGoodMenFounder: `Founder`,
+            aFewGoodMenFounderAchievements: `Alexandru is a Forbes Europe 30 under 30 nominee in industry category. In 2013, he was nominated by Time Magazine as one of the most influential teens of the world. MIT named an asteroid with his name.`,
+
+            powerToThePeopleContactTheCommunity: `Contact`,
+            powerToThePeopleTalkToTheCommunity: `Let's Talk`,
+            powerToThePeopleWaysToContact: `Here are the ways you can contact us with any questions you have`,
+
+            messageReceivedWaitNow: `We received your message and will contact you back soon.`,
+            inputYourName: `Your name`,
+            inputYourEmail: `Your email`,
+            inputYourMessage: `Message`,
+            hitSendHard: `SEND`,
+        },
+
+        faq:{
+            weGotAnswers: `Frequently Asked Questions`,
+            weGotAllTheAnswers: `Got a question? We are here to answer your questions!`,
+
+            questionTypeOfPaymentAccepted: `¿Qué pagos serán aceptados?`,
+            answerTypeOfPaymentAccepted: `Todos los pagos serán aceptados, excepto los pagos que ofrecen devoluciones de cargo fácilmente estafando a los vendedores. ${config.app.name} no está limitado y los usuarios pueden definir sus propios métodos de pago, incluidos: cara a cara, transferencias bancarias nacionales / internacionales, SEPA, AliPay, WeChat Pay, Revolut, BTC, ETH, WEBD, etc. Se desaconsejan algunos pagos que facilitan las devoluciones de cargo para compradores de bajo rango. Uno de estos pagos es PayPal porque es fácil hacer devoluciones de cargo alegando que no recibió los productos.`,
+
+            questionPeerToPeerExchange: `¿Cómo funciona el intercambio Peer To Peer?`,
+            answerPeerToPeerExchange: `Se produce un intercambio cuando un comprador adquiere de un vendedor algunas monedas / fichas. El vendedor abrirá una operación que vende algunos activos (tokens) y el comprador paga fuera de la cadena para adquirir estos activos (tokens). Después de finalizar y confirmar el pago fuera de la cadena del comprador, el comprador obtendrá los activos (tokens) del vendedor.`,
+
+            questionIsBuyerProtected: `¿Cómo esta protegido el comprador en ${config.app.name} ?`,
+            answerIsBuyerProtected: `El Vendedor que posee los activos y desea venderlos en el intercambio tendrá que depositar anónimamente los activos en una cuenta comercial especial que aún es de su propiedad. Luego, el vendedor establece un depósito oculto a esta cuenta comercial para la supervisión comercial. Ahora, el vendedor no puede mover fondos de la cuenta comercial sin el permiso del intermediario. En caso de disputa, el comprador puede presentar una queja demostrando que envió los fondos al vendedor, pero el vendedor no se los envío voluntariamente.`,
+
+            questionCompletelyBlindedEscrow: `¿Qué es un fideicomiso completamente ciego?`,
+            answerCompletelyBlindedEscrow: `Un fideicomiso completamente ciego es un intermediario que NO sabrá la cantidad negociada, la dirección del vendedor real y la dirección del comprador real. Tampoco tiene poder para mover los fondos por sí mismo, solo se usa para asegurar que el comprador no mueva los fondos prometidos a un vendedor.`,
+
+            questionIsTradeChatEncrypted: `¿Los mensajes de chat están encriptados en las transacciones?`,
+            answerIsTradeChatEncrypted: `SI! Todos los mensajes de chat se cifran de extremo a extremo utilizando funciones criptográficas.`,
+
+            questionAddressNeedToKnowOnly: `¿El comprador, el vendedor o el fideicomiso conocerán las direcciones?`,
+            answerAddressNeedToKnowOnly: `¡No! El comprador no sabrá la dirección real del vendedor. El vendedor no sabrá la dirección real del comprador. El Fideicomiso no conocerá las direcciones reales del Comprador o Vendedor ni el monto de la transacción. El comprador y el vendedor solo sabrán la dirección del intermediario.`,
+
+            questionPromiseFulfilled: `¿Cómo sabe el Comprador que el Vendedor tiene suficientes fondos prometidos para vender?`,
+            answerPromiseFulfilled: `Si acordaron el precio, el comprador solicitará automáticamente al Vendedor una prueba de que tiene fondos suficientes para realizar el intercambio. El vendedor generará una prueba que demuestre que tiene suficiente para el intercambio.`,
+
+            questionCanWeTradeAnything: `¿Qué activos / tokens se pueden intercambiar?`,
+            answerCanWeTradeAnything: `Cualquier activo del mundo real puede "tokenizarse" en forma digital y negociarse en  ${config.app.name}. Cualquiera puede emitir activos / tokens en ${config.app.name}. Los activos / tokens pueden ser vinculados por un activo del mundo real, pueden ser garantizados por un activo / cripto diferente o incluso no garantizados.`,
+
+            questionRingSignatureUntraceable: `¿Cómo no se pueden rastrear los activos / tokens de  ${config.app.name} ?`,
+            answerRingSignatureUntraceable: `${config.app.name}utiliza firmas de anillo confidenciales (como en Monero), lo que significa que las cantidades están ocultas (confidenciales) y una transacción utiliza señuelos aleatorios de la red (una transacción de anillo).`,
+        },
+
+    },
+
+    layout:{
+
+        header:{
+
+            navbar:{
+                ppLogoInline: `logo`,
+                ppLogoInlineWhite: `logo`,
+                goGoHome: `Home`,
+                goGoBlockExplorer: `Block Explorer`,
+                goGoWebWallet: `Web Wallet`,
+                goGoContact: `Contact`,
+                goGoFaq: `FAQ`,
+            },
+
+        },
+
+        footer:{
+
+        },
+
+    }
+
+
+
+}
